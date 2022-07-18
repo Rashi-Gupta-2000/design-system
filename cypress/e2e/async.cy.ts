737fd34a7f3e3ceac6165bfb322b885ddbbf9de0
Rashi-Gupta-2000 <rashi.gupta@innovaccer.com>
2022-07-18 18:27:37 +0530

    feat: integrate cypress test

---
+++ cypress/e2e/async.cy.ts
@@ -0,0 +1,13 @@
+describe('My First Test', () => {
+    it('Visits the design system', async () => {
+        cy.visit('https://design.innovaccer.com/components/overview/all-components/')
+        await new Cypress.Promise<string>(() => {
+            cy.get('.MenuItem.MenuItem--vertical.MenuItem--expanded')
+            .each(($el) => {
+                cy.wrap($el).then(($el) => cy.wrap($el).click())
+            })
+            
+        })
+        // cy.log(text)
+    })
+})
